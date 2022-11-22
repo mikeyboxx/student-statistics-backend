8bde92b10e928741ec80e2ad2e552ccd98951c38
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-22 17:23:47 -0500

    Changed connection.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const routes = require('./routes');
 const PORT = process.env.port || 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

9085591055aa4d70005d7afc07781d1acf5cf9e3
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-23 15:28:18 -0500

    Added parameters to keep connection alive on heroku

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,14 @@ const { connect, connection } = require('mongoose');
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
+  // 'mongodb+srv://DB_Admin:Injustice1@cluster0.21lhvfj.mongodb.net/?retryWrites=true&w=majority';
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
 });
 
 module.exports = connection;
